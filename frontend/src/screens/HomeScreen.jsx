289457ba4461fdc8675218a4f4e23820166da30e
Abe Pasion <pasionabe@gmail.com>
2024-03-02 11:30:41 -0500

    Implements Paginate Component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,11 +3,12 @@ import { useParams } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Paginate from '../components/Paginate';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
   const { pageNumber } = useParams();
-  
+
   const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
@@ -23,6 +24,11 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
+          <Paginate 
+            pages={data.pages} 
+            page={data.page} 
+            isAdmin={false}
+          />
         </>)}
     </>
   )
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,13 +1,17 @@
 import { LinkContainer } from 'react-router-bootstrap'
 import { Table, Button, Row, Col } from 'react-bootstrap'
 import { FaEdit, FaTrash } from 'react-icons/fa'
+import { useParams } from 'react-router-dom'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
+import Paginate from '../../components/Paginate'
 import { useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation } from '../../slices/productsApiSlice'
 import { toast } from 'react-toastify'
 
 const ProductListScreen = () => {
-    const { data: products, error, isLoading, refetch } = useGetProductsQuery();
+    const { pageNumber } = useParams();
+
+    const { data, error, isLoading, refetch } = useGetProductsQuery({ pageNumber });
 
     const [createProduct, {isLoading: loadingCreate} ] = useCreateProductMutation();
 
@@ -62,7 +66,7 @@ const ProductListScreen = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {products.map((product) => (
+                    {data.products.map((product) => (
                         <tr key={product._id}>
                             <td>{product._id}</td>
                             <td>{product.name}</td>
@@ -83,6 +87,11 @@ const ProductListScreen = () => {
                     ))}
                 </tbody>
             </Table>
+            <Paginate 
+                pages={data.pages} 
+                page={data.page} 
+                isAdmin={true}
+            />
         </>
     )}
   </>
